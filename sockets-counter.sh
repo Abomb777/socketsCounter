83e9a458c50c5c25b1bf49d2e9a0fec95033c99a
Anton Turbin <abomb82@gmail.com>
2021-06-19 20:33:58 +0300

    linux calc

--- sockets-counter.sh
+++ sockets-counter.sh
@@ -5,6 +5,15 @@ total=`netstat -ant | awk 'NR>2' | awk '{print $7}' | sort | uniq -c`
 
 echo "Total: ${total}"
 
+range=`cat /proc/sys/net/ipv4/ip_local_port_range`
+therange=`echo $range | awk '{print $1}'`
+totalsockets=`echo $range | awk '{print $2}'`
+timelimit=`cat /proc/sys/net/ipv4/tcp_fin_timeout`
+echo Total max sockets per sec $((($totalsockets-$therange) / $timelimit))
+echo Free sockets $(((($totalsockets-$therange) / $timelimit)-$total))
+
+#echo "${range}"
+
 
 #netstat -ant | awk 'NR>2'| awk '{print $6}' | sort | uniq -c | sort -n | while read count name
 #do
